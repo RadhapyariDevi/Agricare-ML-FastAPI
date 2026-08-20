c8d67d1b71540df822e89241b3b64f6580fcc9a0
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-08-20 12:46:20 +0530

    add get diagnosis history endpoint

--- backend/src/routes/diagnosis.router.js
+++ backend/src/routes/diagnosis.router.js
@@ -2,9 +2,11 @@ import express from "express";
 import { analyze } from "../controllers/diagnosis.controller.js";
 import upload from "../middleware/upload.middleware.js";
 import { protect } from "../middleware/auth.middleware.js";
+import { getDiagnosisHistory } from "../controllers/diagnosisHistory.controller.js";
 
 const router = express.Router();
 
 router.post("/analyze", protect, upload.single("image"), analyze);
+router.get("/history", protect, getDiagnosisHistory);
 
 export default router;
